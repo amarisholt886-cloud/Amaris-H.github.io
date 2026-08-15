743050670b34734bf6773fec57053741d2fc3a22
amarisholt886-cloud <amarisholt886@gmail.com>
2026-08-14 22:57:33 -0400

    Update script.js

--- script.js
+++ script.js
@@ -3,23 +3,9 @@ KEY FEATURE: Personalize it
 https://stackoverflow.com/questions/26107125/cannot-read-property-addeventlistener-of-null
 */
 
-//Move these variables to the top
-var factList = [
-  "Males were more likely to perpetuate toxic behavior, while female players were more likely to withdraw.", 
-  "Players with higher levels of impulsivity and narcissism as well as those who are motivated by destruction or frequently engage in competitive brackets were more likely to engage in hateful conduct than those with more positive personality traits and broader motivations for play.", 
-  "Women, nonheterosexual people and individuals with disabilities were more likely to be harassed.", 
-  "Teens laughed off events more often than adult players, showing a generational difference between those growing up in an era of established toxic gamer culture and those who did not.", 
-  "A large majority of adolescent players had been bystanders to hate-based harassment, and more than a third reported being a victim.", 
-  "Participants responded to hate speech either by calling out and reporting an inflammatory event and supporting the victim or by withdrawing from the game and interaction completely."
-];
-
-
 
 // This wrapper forces the computer to wait for the HTML buttons to exist!
 var scriptBtn = document.getElementById("scriptBtn");
-var factBtn = document.getElementById("factBtn");
-var fact = document.getElementById("fact");
-var count = 0;
 
   if (scriptBtn) {
     scriptBtn.addEventListener("click", generateScript);
@@ -33,6 +19,19 @@ function generateScript() {
     displayScript.innerHTML = "I, " + name + ", pledge to never participate in the hate or harassment in gaming!";
   }
 }
+
+var factList = [
+  "Males were more likely to perpetuate toxic behavior, while female players were more likely to withdraw.", 
+  "Players with higher levels of impulsivity and narcissism as well as those who are motivated by destruction or frequently engage in competitive brackets were more likely to engage in hateful conduct than those with more positive personality traits and broader motivations for play.", 
+  "Women, nonheterosexual people and individuals with disabilities were more likely to be harassed.", 
+  "Teens laughed off events more often than adult players, showing a generational difference between those growing up in an era of established toxic gamer culture and those who did not.", 
+  "A large majority of adolescent players had been bystanders to hate-based harassment, and more than a third reported being a victim.", 
+  "Participants responded to hate speech either by calling out and reporting an inflammatory event and supporting the victim or by withdrawing from the game and interaction completely."
+];
+
+var factBtn = document.getElementById("factBtn");
+var fact = document.getElementById("fact");
+var count = 0;
 
 if (factBtn) {
   factBtn.addEventListener("click", displayFact);
